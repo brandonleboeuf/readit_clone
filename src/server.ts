870974486a6937e369ea13519e19fbdb7635ccbb
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2021-02-02 13:46:05 -0800

    adds logout and me routes

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,33 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import express from 'express'
-import morgan from 'morgan'
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
-import authRoutes from './routes/auth'
- 
-import trim from './middleware/trim'
+dotenv.config();
 
-const app = express()
+import authRoutes from './routes/auth';
 
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(trim)
+import trim from './middleware/trim';
 
-app.get('/', (_, res) => res.send("Hello World!!"))
-app.use('/api/auth', authRoutes)
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(trim);
+app.use(cookieParser());
+
+app.get('/', (_, res) => res.send('Hello World!!'));
+app.use('/api/auth', authRoutes);
 
 app.listen(5000, async () => {
-  console.log(`Server running at http://localhost:5000`)
+  console.log(`Server running at http://localhost:5000`);
 
   try {
-    await createConnection()
-    console.log("Database connected!")
+    await createConnection();
+    console.log('Database connected!');
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-})
+});
