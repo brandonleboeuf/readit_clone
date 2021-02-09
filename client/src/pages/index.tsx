5c221e73aa90cefba4f8567e602135bc685896ef
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2021-02-08 17:23:05 -0800

    adds action links and icons on bottom of post

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -65,6 +65,29 @@ export default function Home() {
                   <a className="my-1 text-lg font-medium">{post.title}</a>
                 </Link>
                 {post.body && <p className="my-1 text-sm">{post.body}</p>}
+
+                <div className="flex">
+                  <Link href={post.url}>
+                    <a>
+                      <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                        <span className="font-bold">20 comments</span>
+                      </div>
+                    </a>
+                  </Link>
+                  <a>
+                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+                      <i className="mr-1 fas fa-share fa-xs"></i>
+                      <span className="font-bold">share</span>
+                    </div>
+                  </a>
+                  <a>
+                    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+                      <i className="mr-1 fas fa-bookmark fa-xs"></i>
+                      <span className="font-bold">save</span>
+                    </div>
+                  </a>
+                </div>
               </div>
             </div>
           ))}
