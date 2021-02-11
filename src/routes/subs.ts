ba8d6b084649b8add1b28dc20b88c14c355b2316
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2021-02-10 16:50:28 -0800

    allows user to upload images

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,77 +1,159 @@
-import { Router, Request, Response } from 'express';
-import { isEmpty } from 'class-validator';
-import { getRepository } from 'typeorm';
+import { Router, Request, Response } from 'express'
+import { isEmpty } from 'class-validator'
+import { getRepository } from 'typeorm'
+import multer, { FileFilterCallback } from 'multer'
+import path from 'path'
+import fs from 'fs'
 
-import User from '../entities/User';
-import Sub from '../entities/Sub';
+import User from '../entities/User'
+import Sub from '../entities/Sub'
 
-import auth from '../middleware/auth';
-import user from '../middleware/user';
-import Post from '../entities/Post';
+import auth from '../middleware/auth'
+import user from '../middleware/user'
+import Post from '../entities/Post'
+import { makeId } from '../util/helpers'
+import { NextFunction } from 'express'
 
 const createSub = async (req: Request, res: Response) => {
-  const { name, title, description } = req.body;
+  const { name, title, description } = req.body
 
-  const user: User = res.locals.user;
+  const user: User = res.locals.user
 
   try {
-    let errors: any = {};
-    if (isEmpty(name)) errors.name = 'Name must not be empty';
-    if (isEmpty(title)) errors.title = 'Title must not be empty';
+    let errors: any = {}
+    if (isEmpty(name)) errors.name = 'Name must not be empty'
+    if (isEmpty(title)) errors.title = 'Title must not be empty'
 
     const sub = await getRepository(Sub)
       .createQueryBuilder('sub')
       .where('lower(sub.name) = :name', { name: name.toLowerCase() })
-      .getOne();
+      .getOne()
 
-    if (sub) errors.name = 'Sub exists already';
+    if (sub) errors.name = 'Sub exists already'
 
     if (Object.keys(errors).length > 0) {
-      throw errors;
+      throw errors
     }
   } catch (err) {
-    return res.status(400).json(err);
+    return res.status(400).json(err)
   }
 
   try {
-    const sub = new Sub({ name, description, title, user });
-    await sub.save();
+    const sub = new Sub({ name, description, title, user })
+    await sub.save()
 
-    return res.json(sub);
+    return res.json(sub)
   } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Something went wrong' });
+    console.log(err)
+    return res.status(500).json({ error: 'Something went wrong' })
   }
-};
+}
 
 const getSub = async (req: Request, res: Response) => {
-  const name = req.params.name;
+  const name = req.params.name
 
   try {
-    const sub = await Sub.findOneOrFail({ name });
+    const sub = await Sub.findOneOrFail({ name })
     const posts = await Post.find({
       where: { sub },
       order: { createdAt: 'DESC' },
       relations: ['comments', 'votes'],
-    });
+    })
 
-    sub.posts = posts;
+    sub.posts = posts
 
     // if the user is logged in, each post will show if they have voted it up or down (if at all)
     if (res.locals.user) {
-      sub.posts.forEach((p) => p.setUserVote(res.locals.user));
+      sub.posts.forEach((p) => p.setUserVote(res.locals.user))
     }
 
-    return res.json(sub);
+    return res.json(sub)
   } catch (err) {
-    console.log(err);
-    return res.status(404).json({ error: 'Sub not found' });
+    console.log(err)
+    return res.status(404).json({ error: 'Sub not found' })
   }
-};
+}
 
-const router = Router();
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user
 
-router.post('/', user, auth, createSub);
-router.get('/:name', user, getSub);
+  try {
+    const sub = await Sub.findOneOrFail({ where: { name: req.params.name } })
 
-export default router;
+    if (sub.username !== user.username) {
+      return res.status(403).json({ error: "You don't own this sub" })
+    }
+
+    res.locals.sub = sub
+    return next()
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ error: 'something went wrong' })
+  }
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: 'public/images',
+    filename: (_, file, callback) => {
+      const name = makeId(15)
+      callback(null, name + path.extname(file.originalname))
+    },
+  }),
+  fileFilter: (_, file: any, callback: FileFilterCallback) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+      callback(null, true)
+    } else {
+      callback(new Error('Not an image'))
+    }
+  },
+})
+
+const uploadSubImage = async (req: Request, res: Response) => {
+  const sub: Sub = res.locals.sub
+  try {
+    const type = req.body.type
+
+    if (type !== 'image' && type !== 'banner') {
+      // if the file is of the wrong type, it will be removed from the images folder
+      fs.unlinkSync(req.file.path)
+      return res.status(400).json({ error: 'Invalid type' })
+    }
+
+    let oldImageUrn: string = ''
+    if (type === 'image') {
+      oldImageUrn = sub.imageUrn || ''
+      sub.imageUrn = req.file.filename
+    } else if (type === 'banner') {
+      oldImageUrn = sub.bannerUrn || ''
+      sub.bannerUrn = req.file.filename
+    }
+
+    await sub.save()
+
+    // deletes old image
+    if (oldImageUrn !== '') {
+      fs.unlinkSync(`public/images/${oldImageUrn}`)
+    }
+
+    return res.json(sub)
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ error: 'Something went wrong' })
+  }
+}
+
+const router = Router()
+
+router.post('/', user, auth, createSub)
+router.get('/:name', user, getSub)
+router.post(
+  '/:name/image',
+  user,
+  auth,
+  ownSub,
+  upload.single('file'),
+  uploadSubImage
+)
+
+export default router
