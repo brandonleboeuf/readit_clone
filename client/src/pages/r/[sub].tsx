55c37842cff923353cb074467f67008d287d2acd
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2021-02-20 16:08:56 -0800

    changes backgroundsize to look better

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -103,7 +103,7 @@ export default function SubPage() {
                   style={{
                     backgroundImage: `url(${sub.bannerUrl})`,
                     backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'contain',
+                    backgroundSize: 'cover',
                     backgroundPosition: 'center',
                   }}
                 ></div>
